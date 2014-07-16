d571adc91df5c3f73be58ffb08af320854bc77f6
ltigue <moneynloans@yahoo.com>
2014-07-16 16:07:06 -0400

    July 16th practice changes
    example project

--- Person/Person/Program.cs
+++ Person/Person/Program.cs
@@ -4,33 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Friend
+namespace Car
 
 {
-    class Person 
-    {
-        public string people { get; set; }
-    }
+   
        
 
     class Program
     {
-        static void Main(string[] args)
+        public partial class Form1:Form
         {
-            
-            Person[]people = new Person [4];
-             var n = 0;
-            do
-            {
-               
-                people[n]=new Person();
-                n++;
-               
-            }
-            while (n < 4);
+            int startingMileage;
+            int endingMileage;
+            double milesTraveled;
+            double reimburseRate = .39;
+            double amountOwed;
 
-            Console.WriteLine(people);
-            Console.ReadLine();
+            public Form1()
+            {
+                private void button1_Click(object sender, EventArgs e)
+                {
+                startingMileage =(int)numericUpDown1.Value;
+                endingMileage=(int)numericUpDown2.Value;
+
+                    if (startingMileage< endingMileage)
+                    {
+                        milesTraveled=endingMileage-=startingMileage;
+                        amountOwed= milesTraveled*=reimburseRate;
+                        label4.Text="$" + amountOwed;
+	{
+		 
+	}
+
+                    }
+                    else
+                    {
+                    MessageBox.Show("Ha ha");
+                    }
+
+
+        
+                }
+            }
 
             
 
