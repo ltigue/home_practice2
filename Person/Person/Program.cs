d12aafbdb4d7411bee096926774472f989ba24aa
ltigue <moneynloans@yahoo.com>
2014-07-16 06:28:30 -0400

    changes
    practice exercises

---
+++ Person/Person/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Friend
+
+{
+    class Person 
+    {
+        public string people { get; set; }
+    }
+       
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person[]people = new Person [4];
+             var n = 0;
+            do
+            {
+               
+                people[n]=new Person();
+                n++;
+               
+            }
+            while (n < 4);
+
+            Console.WriteLine(people);
+            Console.ReadLine();
+
+            
+
+
+        }
+    }
+}
---
+++ pins_example/pins_example/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Array;
+
+namespace pins_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pins = { 3, 2, 1, 5, 6 };
+            int[] push = pins;
+            Array.Reverse(pins);
+            Console.WriteLine(push);
+        }
+    }
+}
